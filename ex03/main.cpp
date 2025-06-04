d14eef0d678356e4de2687e8a2ffea890d4886da
root <sushi.honda@gmail.com>
2025-06-04 14:49:46 +0800

    ex03) refactor: try-catch in main() for new error
      NULL returned from object should be distinguished condition error or new malloc error.
      logic error should be handled in logic not exeption
      exception should be used for uncertain error

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 19:36:03 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/03 19:11:50 by athonda          ###   ########.fr       */
+/*   Updated: 2025/06/04 14:46:13 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,49 +24,56 @@ void	printBanner(std::string const &title)
 
 int	main(void)
 {
-	printBanner("simple test");
-	Bureaucrat	minister("First secretary", 5);
-	Bureaucrat	officer("last secretary", 150);
-	RobotomyRequestForm document("home");
-	std::cout << document << std::endl;
-	officer.executeForm(document);
-	minister.signForm(document);
-	std::cout << document << std::endl;
-	officer.executeForm(document);
-	minister.executeForm(document);
+	try
+	{
+		printBanner("simple test");
+		Bureaucrat	minister("First secretary", 5);
+		Bureaucrat	officer("last secretary", 150);
+		RobotomyRequestForm document("home");
+		std::cout << document << std::endl;
+		officer.executeForm(document);
+		minister.signForm(document);
+		std::cout << document << std::endl;
+		officer.executeForm(document);
+		minister.executeForm(document);
 
-	printBanner("Shall we hire a intern?");
-	Intern	boy;
-	AForm	*doc;
-	doc = boy.makeForm("vacation application form", "myself");
-	doc = boy.makeForm("Robotomy", "Christopher");
-	std::cout << *doc << std::endl;
-	officer.executeForm(*doc);
-	minister.signForm(*doc);
-	std::cout << *doc << std::endl;
-	officer.executeForm(*doc);
-	minister.executeForm(*doc);
-	delete doc;
+		printBanner("Shall we hire a intern?");
+		Intern	boy;
+		AForm	*doc;
+		doc = boy.makeForm("vacation application form", "myself");
+		doc = boy.makeForm("Robotomy", "Christopher");
+		std::cout << *doc << std::endl;
+		officer.executeForm(*doc);
+		minister.signForm(*doc);
+		std::cout << *doc << std::endl;
+		officer.executeForm(*doc);
+		minister.executeForm(*doc);
+		delete doc;
 
-	printBanner("Boy!! make 1000 documents until tomorrow morning!!");
-	doc = boy.makeForm("Shrubbery", "Mrs.Robinson");
-	std::cout << *doc << std::endl;
-	officer.executeForm(*doc);
-	minister.signForm(*doc);
-	std::cout << *doc << std::endl;
-	officer.executeForm(*doc);
-	minister.executeForm(*doc);
-	delete doc;
+		printBanner("Boy!! make 1000 documents until tomorrow morning!!");
+		doc = boy.makeForm("Shrubbery", "Mrs.Robinson");
+		std::cout << *doc << std::endl;
+		officer.executeForm(*doc);
+		minister.signForm(*doc);
+		std::cout << *doc << std::endl;
+		officer.executeForm(*doc);
+		minister.executeForm(*doc);
+		delete doc;
 
-	printBanner("Do you know the photo copy machine is called Xerox?");
-	doc = boy.makeForm("Presidential", "Donald Trump");
-	std::cout << *doc << std::endl;
-	officer.executeForm(*doc);
-	minister.signForm(*doc);
-	std::cout << *doc << std::endl;
-	officer.executeForm(*doc);
-	minister.executeForm(*doc);
-	delete doc;
+		printBanner("Do you know the photo copy machine is called Xerox?");
+		doc = boy.makeForm("Presidential", "Donald Trump");
+		std::cout << *doc << std::endl;
+		officer.executeForm(*doc);
+		minister.signForm(*doc);
+		std::cout << *doc << std::endl;
+		officer.executeForm(*doc);
+		minister.executeForm(*doc);
+		delete doc;
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 
 	return (0);
 }
